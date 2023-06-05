632eda5fc9589a03affe6d219e3668b9ed3a8a15
mutazabutaha <mutazabutaha13@outlook.com>
2023-06-05 22:25:56 +0000

    code13

--- stack-queue-brackets/stack_queue_brackets.py
+++ stack-queue-brackets/stack_queue_brackets.py
@@ -1,22 +1,5 @@
-# def validate_brackets(str):
-#     stack = []
-#     brackets = {"(": ")", "{": "}", "[": "]"}
 
-#     for char in str:
-#         if char in brackets.keys():
-#             stack.append(char)
-#         elif char in brackets.values():
-#             if not stack:
-#                 return False
-#             if char != brackets[stack.pop()]:
-#                 return False
-
-#         return True
-
-
-# validate_brackets("()")
-
-def is_balanced(string):
+def validate_brackets(string):
     stack = []
     brackets = {"(": ")", "{": "}", "[": "]"}
     
@@ -32,10 +15,10 @@ def is_balanced(string):
     return len(stack) == 0
 
 string1 = "(qqqqqq){sssss}[qqqqq]"  # Balanced brackets
-print(is_balanced(string1))  # Output: True
+print(validate_brackets(string1))  # Output: True
 
 string2 = "({)}"  # Unbalanced brackets
-print(is_balanced(string2))  # Output: False
+print(validate_brackets(string2))  # Output: False
 
 string3 = "[{aaaaaaaa}[(Ssssss)]"  # Balanced brackets
-print(is_balanced(string3))  # Output: True
+print(validate_brackets(string3))  # Output: True
--- stack-queue-brackets/test_stack_queue_brackets.py
+++ stack-queue-brackets/test_stack_queue_brackets.py
@@ -1,26 +1,26 @@
 import pytest 
-from stack_queue_brackets import is_balanced
+from stack_queue_brackets import validate_brackets
 
-def test_is_balanced():
-    output = is_balanced("(qqqqqq){sssss}[qqqqq]")
+def test_validate_brackets():
+    output = validate_brackets("(qqqqqq){sssss}[qqqqq]")
     
     assert output ==  True
 
 
 
-def test_is_balanced2():
-    output = is_balanced("({)}")
+def test_validate_brackets2():
+    output = validate_brackets("({)}")
     
     assert output ==  False
 
 
-def test_is_balanced3():
-    output = is_balanced("[{aaaaaaaa}[(Ssssss)]")
+def test_validate_brackets3():
+    output = validate_brackets("[{aaaaaaaa}[(Ssssss)]")
    
     assert output ==  False
 
-def test_is_balanced4():
-    output = is_balanced("[{aaaaaaaa}(Ssssss){}]")
+def test_validate_brackets4():
+    output = validate_brackets("[{aaaaaaaa}(Ssssss){}]")
    
     assert output ==  True
 
